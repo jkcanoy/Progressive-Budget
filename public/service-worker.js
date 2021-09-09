21ea4d12aef9990e46913968cd65e3ffe8e5ba9e
Kyle Canoy <kcanoy@uw.edu>
2021-09-09 16:38:34 -0700

    update files_to_cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,8 @@
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
+  "./assets/js/index.js",
+  "./assets/js/indexedDB.js",
   "./assets/css/styles.css",
   "./assets/icons/icon-192x192.png",
   "./assets/icons/icon-512x512.png",
