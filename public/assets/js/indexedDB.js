df86ad6f45003464369a87976582bb15b286bdfe
Kyle Canoy <kcanoy@uw.edu>
2021-09-08 22:05:29 -0700

    finish indexedDB js

--- public/assets/js/indexedDB.js
+++ public/assets/js/indexedDB.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 let db;
 const request = window.indexedDB.open("budget-tracker, 1");
 
@@ -36,4 +38,29 @@ function saveRecord(record) {
 
 function checkDatabase() {
   const transaction = db.transaction(["PendingStore"], "readwrite");
+  const store = transaction.objectStore("PendingStore");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          if (response.length !== 0) {
+            transaction = db.transaction(["PendingStore"], "readwrite");
+            const currentStore = transaction.objectStore("PendingStore");
+            currentStore.clear();
+          }
+        });
+    }
+  };
 }
+
+window.addEventListener("online", checkDatabase);
