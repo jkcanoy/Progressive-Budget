3e7427b15382c09d8d3651b7d5ca8971017c4664
Kyle Canoy <kcanoy@uw.edu>
2021-09-09 17:14:22 -0700

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const config = {
   entry: "./public/assets/js/index.js",
   output: {
-    path: __dirname + "./public/dist",
+    path: __dirname + "/public/dist",
     filename: "[name].bundle.js",
   },
   mode: "development",
